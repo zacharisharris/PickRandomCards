f5685f9ea5fee416c60f99d90996f5107bd93978
Harris Zacharis <zacharisharris@gmail.com>
2020-08-11 15:17:46 +0300

    Add project files.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+
+            return pickedCards;
+        }
+
+        static string RandomSuit()
+        {
+            int value = random.Next(1, 4);
+
+            if (value == 1) { return "Spades"; }
+            if (value == 2) { return "Hearts"; }
+            if (value == 3) { return "Spades"; }
+
+            return "Diamonds";
+        }
+
+        static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+
+            if (value == 1) { return "Ace"; }
+            if (value == 11) { return "Jack"; }
+            if (value == 12) { return "Queen"; }
+            if (value == 13) { return "King"; }
+
+            return value.ToString();
+
+        }
+    }
+}
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many cards shall I pick?");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (var card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The number you entered was not valid.");
+            }
+        }
+    }
+}
